87245138c50989aad3f0425df70bf1795ec79118
WKupper <34351127+WKupper@users.noreply.github.com>
2022-04-13 23:07:30 -0400

    HTTP Build Fix

--- setupscripts/httpbuild.sh
+++ setupscripts/httpbuild.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-if [$HTTPS_TOGGLE = 1]
+if [$HTTPS_TOGGLE = "1"]
 then
 	cp -f /etc/httpd/httpd-ssl.conf /etc/httpd/conf/httpd.conf
 	aws s3 cp s3://$S3_BUCKET_NAME/$S3_ENVIRONMENT/moodle/$S3_CERT_NAME /etc/pki/tls/certs/cert.pem
     aws s3 cp s3://$S3_BUCKET_NAME/$S3_ENVIRONMENT/moodle/$S3_PRIVATE_KEY /etc/pki/tls/certs/privatekey.pem
 	
-	if [$SSL_CHAIN_TOGGLE = 1]
+	if [$SSL_CHAIN_TOGGLE = "1"]
 	then
 		aws s3 cp s3://$S3_BUCKET_NAME/$S3_ENVIRONMENT/moodle/$S3_CHAIN_CERT /etc/pki/tls/certs/chain.pem
 		chown apache:apache /etc/pki/tls/certs/chain.pem
